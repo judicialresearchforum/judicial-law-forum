057974626a6b52d2c5104200689c30c6ebe22415
judicialresearchforum <judicialresearchforum26@gmail.com>
2026-06-08 10:58:13 +0545

    Update server.js

--- server.js
+++ server.js
@@ -20,17 +20,17 @@ app.post("/contact", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      },
-      connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 10000
-    });
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 60000,
+  greetingTimeout: 60000,
+  socketTimeout: 60000
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
